3bfd4b1d50a16691e16ed5b6329ee93f3610e0bc
webmonarch <eric@collectivegenius.net>
2012-05-03 22:33:32 -0700

    adding documentation for d34r selection APIs

---
+++ src/raphael/_documentation.js
@@ -0,0 +1,16 @@
+// This is a dummy file to help JSDoc document the d34raphael code better.
+//
+// It is not intented to be included or compiled.
+
+/**
+ * @namespace
+ */
+d3 = {};
+
+/**
+ * @class Raphael aware selection, intented to be used much like <code><a href="https://github.com/mbostock/d3/wiki/Selections">d3.selection</a></code>.
+ *  caveats and differences documented below.  If you don't see a method documented, it probably doesn't exist yet.
+ *
+ * @constructor
+ */
+D3RaphaelSelection = function() {};
