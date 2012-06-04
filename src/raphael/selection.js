84de50cace5c91ce15eb29e9db9fca3043755909
Clint Tseng <clint@dontexplain.com>
2012-06-04 15:46:21 -0700

    update raphael selection.data() to reuse default selection.data() code more.

--- src/raphael/selection.js
+++ src/raphael/selection.js
@@ -24,117 +24,28 @@ var d3_raphael_selectionPrototype = [];
  * @name D3RaphaelSelection#data
  */
 d3_raphael_selectionPrototype.data = function(value, key_function) {
-    var i = -1,
-        n = this.length,
-        group,
-        node;
+    // kind of a hack, but saves a lot of code duplication; sub out the
+    // built-in selection calls for our own for the duration of the inner call.
 
-    // If no value is specified, return the first value.
-    if (!arguments.length) {
-        value = new Array(n = (group = this[0]).length);
-        while (++i < n) {
-            if (node = group[i]) {
-                value[i] = node.__data__;
-            }
-        }
-        return value;
-    }
+    // save old
+    var old_d3_selection_enter = d3_selection_enter,
+        old_d3_selection = d3_selection;
 
-    function bind(group, groupData) {
-        var i,
-            n = group.length,
-            m = groupData.length,
-            n0 = Math.min(n, m),
-            n1 = Math.max(n, m),
-            updateNodes = [],
-            enterNodes = [],
-            exitNodes = [],
-            node,
-            nodeData;
+    // sub in
+    var selection = this;
+    d3_selection_enter = function(elems) {
+        return d3_raphael_enterSelection(elems, selection.root);
+    };
+    d3_selection = function(elems) {
+        return d3_raphael_selection(elems, selection.root);
+    };
 
-        if (key_function) {
-            var nodeByKeyValue = new d3_Map,
-                keyValues = [],
-                keyValue,
-                j = groupData.length;
+    // actual call
+    var update = d3_selectionPrototype.data.call(this, value, key_function);
 
-            for (i = -1; ++i < n;) {
-                keyValue = key_function.call(node = group[i], node.__data__, i);
-                if (nodeByKeyValue.has(keyValue)) {
-                    exitNodes[j++] = node; // duplicate key
-                } else {
-                    nodeByKeyValue.set(keyValue, node);
-                }
-                keyValues.push(keyValue);
-            }
-
-            for (i = -1; ++i < m;) {
-                keyValue = key_function.call(groupData, nodeData = groupData[i], i)
-                if (nodeByKeyValue.has(keyValue)) {
-                    updateNodes[i] = node = nodeByKeyValue.get(keyValue);
-                    node.__data__ = nodeData;
-                    enterNodes[i] = exitNodes[i] = null;
-                } else {
-                    enterNodes[i] = d3_selection_dataNode(nodeData);
-                    updateNodes[i] = exitNodes[i] = null;
-                }
-                nodeByKeyValue.remove(keyValue);
-            }
-
-            for (i = -1; ++i < n;) {
-                if (nodeByKeyValue.has(keyValues[i])) {
-                    exitNodes[i] = group[i];
-                }
-            }
-        } else {
-            for (i = -1; ++i < n0;) {
-                node = group[i];
-                nodeData = groupData[i];
-                if (node) {
-                    node.__data__ = nodeData;
-                    updateNodes[i] = node;
-                    enterNodes[i] = exitNodes[i] = null;
-                } else {
-                    enterNodes[i] = d3_selection_dataNode(nodeData);
-                    updateNodes[i] = exitNodes[i] = null;
-                }
-            }
-            for (; i < m; ++i) {
-                enterNodes[i] = d3_selection_dataNode(groupData[i]);
-                updateNodes[i] = exitNodes[i] = null;
-            }
-            for (; i < n1; ++i) {
-                exitNodes[i] = group[i];
-                enterNodes[i] = updateNodes[i] = null;
-            }
-        }
-
-        enterNodes.update
-            = updateNodes;
-
-        enterNodes.parentNode
-            = updateNodes.parentNode
-            = exitNodes.parentNode
-            = group.parentNode;
-
-        enter.push(enterNodes);
-        update.push(updateNodes);
-        exit.push(exitNodes);
-    }
-
-    var enter = d3_raphael_enterSelection([], this.root),
-        update = d3_raphael_selection([], this.root),
-        exit = d3_raphael_selection([], this.root);
-
-    if (typeof value === "function") {
-        while (++i < n) {
-            bind(group = this[i], value.call(group, group.parentNode.__data__, i));
-        }
-    } else {
-        while (++i < n) {
-            bind(group = this[i], value);
-        }
-    }
+    // sub out
+    d3_selection_enter = old_d3_selection_enter;
+    d3_selection = old_d3_selection;
 
     /**
      * Returns the entering selection: placeholder nodes for each data element for which no corresponding existing DOM element was found in the current selection.
@@ -146,7 +57,8 @@ d3_raphael_selectionPrototype.data = function(value, key_function) {
      * @function
      * @name D3RaphaelUpdateSelection#enter
      */
-    update.enter = function() { return enter; };
+    var enter = update.enter;
+    update.enter = function() { return enter(); };
 
     /**
      * Returns the exiting selection: existing DOM elements in the current selection for which no new data element was found.
@@ -158,7 +70,8 @@ d3_raphael_selectionPrototype.data = function(value, key_function) {
      * @function
      * @name D3RaphaelUpdateSelection#exit
      */
-    update.exit = function() { return exit; };
+    var exit = update.exit;
+    update.exit = function() { return exit(); };
     return update;
 };
 
